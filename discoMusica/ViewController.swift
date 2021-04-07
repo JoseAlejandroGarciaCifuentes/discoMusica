b87e343867fe40e44c07e0337c3b61a334e8e186
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-04-07 13:35:15 +0200

    git init and view structured, needs some freshing so it may look cooler

---
+++ discoMusica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  discoMusica
+//
+//  Created by Alejandro García on 7/4/21.
+//  Copyright © 2021 Alejandro García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
